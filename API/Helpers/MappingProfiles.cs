c8b39163b2cd85fb5c3aca391ac6b4b9c342eca2
DESSEY <DESSEY@DESKTOP-6M5DRHU>
2021-02-03 13:17:20 +0100

    End of sec_4...Generic Repo, Specification Pattern, using Automapper with DTO

---
+++ API/Helpers/MappingProfiles.cs
@@ -0,0 +1,22 @@
+﻿using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Helpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Product, ProductToReturnDto>()
+                .ForMember(destination => destination.ProductBrand, 
+                    option => option.MapFrom(source => source.ProductBrand.Name))
+                .ForMember(d => d.ProductType, opt =>opt.MapFrom(s => s.ProductType.Name))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Helpers;
+using AutoMapper;
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -31,6 +34,9 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(typeof(MappingProfiles));
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => 
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
@@ -47,6 +53,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseStaticFiles();
 
             app.UseAuthorization();
 
