db6206cdbc00608138b9bf4aeabe816b7bd31a15
DESSEY <DESSEY@DESKTOP-6M5DRHU>
2021-03-19 20:19:33 +0100

    added validation Dto classes plus validation msgs, swagger config for JWT Bearer Auth

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,17 @@ namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$", 
+            ErrorMessage = "Password must have atleast 1 UPPERCASE, one lowercase, one special character, one digit and minimum of 6 characters!")]
+        //strong password with 6-10 characters, Mininum(one uppercase, one lowercase, one digit and one special character)
         public string Password { get; set; }
     }
 }
